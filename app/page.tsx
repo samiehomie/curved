7f236daa9f240087a3e0dfef9452f2dd299d1292
SAM <amen03@naver.com>
2023-01-06 16:29:36 +0900

    Add notion api for headless cms

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,50 @@
-import fetchData from '../lib/getData';
+import { getTitle, getTest } from '../lib/getData';
+import { Suspense } from 'react';
+
+async function Post({ page_id }: { page_id: string }) {
+  const results = await getTest({ page_id });
+  return (
+    <div>
+      {results.map((result) => {
+        return (
+          // @ts-ignore
+          <p key={result.id}>{result?.paragraph?.rich_text[0]?.plain_text}</p>
+        );
+      })}
+    </div>
+  );
+}
 
 export default async function Page() {
-  const database = await fetchData();
-  const posts = database.results.map((data) => {
+  const database = await getTitle();
+
+  const titles = database.map((data) => {
     if ('properties' in data) {
       return { id: data.id, content: data.properties };
     }
     return {};
   });
   return (
-    <ul>
-      {posts.map((post) => (
-        // @ts-ignore
-        <li>{post?.content?.document.title[0].plain_text}</li>
-      ))}
-    </ul>
+    <>
+      <h1>TEST2</h1>
+      <ul>
+        {titles.map((data) => {
+          return (
+            <li key={data.id}>
+              <h2>
+                {
+                  // @ts-ignore
+                  data?.content?.document.title[0].plain_text
+                }
+              </h2>
+              <Suspense fallback={<div>Loading...</div>}>
+                {/* @ts-expect-error Server Component */}
+                <Post page_id={data.id as string} />
+              </Suspense>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
--- lib/getData.ts
+++ lib/getData.ts
@@ -4,9 +4,25 @@ const notion = new Client({
   auth: process.env.AUTH_SECRET,
 });
 
-export default async function fetchData() {
+export async function getTitle() {
   const response = await notion.databases.query({
     database_id: process.env.DATABASE_ID as string,
   });
+  return response.results;
+}
+
+export async function getPost({ page_id }: { page_id: string }) {
+  const response = await notion.pages.properties.retrieve({
+    page_id,
+    property_id: 'AwLd',
+  });
   return response;
+}
+
+export async function getTest({ page_id }: { page_id: string }) {
+  const response = await notion.blocks.children.list({
+    block_id: page_id,
+    page_size: 50,
+  });
+  return response.results;
 }
